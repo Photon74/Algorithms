3c9625b482ddee061f9c7243afccfc27e2ad8e53
Kolodinsky <Photon.74@gmail.com>
2021-04-16 20:03:23 +0300

    1

--- Lesson_1/Lesson1.App1/Lesson1.App1.cs
+++ Lesson_1/Lesson1.App1/Lesson1.App1.cs
@@ -2,10 +2,34 @@
 
 namespace Lesson1.App1
 {
-    class Lesson1App1
+    public class Lesson1App1
     {
         static void Main(string[] args)
         {
+        }
+
+        public static bool IsSimple(int n)
+        {
+            int d = 0;
+            int i = 2;
+
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (d == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
