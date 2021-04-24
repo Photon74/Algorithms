33a7d0ce300271c15ed27dcdedb8ea1a3dd37cf1
Kolodinsky <Photon.74@gmail.com>
2021-04-24 21:12:51 +0300

    1

--- Lesson_3/Distance/CalculationMethods.cs
+++ Lesson_3/Distance/CalculationMethods.cs
@@ -4,60 +4,77 @@ namespace Distance
 {
     public class CalculationMethods
     {
-        public static float CountDistanceSimpleClassFloat(PointClass[] pointOne, PointClass[] pointTwo)
+        static int size = 10000;
+        public static float[] CountDistanceSimpleClassFloat(PointClass[] pointOne, PointClass[] pointTwo)
         {
+            int index = 0;
             float x = 0, y = 0;
+            float[] result = new float[size];
             for (int i = 0; i < pointOne.Length; i++)
             {
                 for (int j = pointTwo.Length; j <= 0; j--)
                 {
                     x = pointOne[i].X - pointTwo[j].X;
                     y = pointOne[i].Y - pointTwo[j].Y;
+                    result[index] = MathF.Sqrt((x * x) + (y * y));
+                    index++;
                 }
             }
-            return MathF.Sqrt((x * x) + (y * y));
+            return result;
         }
 
-        public static float CountDistanceSimpleStructFloat(PointStruct<float>[] pointOne, PointStruct<float>[] pointTwo)
+        public static float[] CountDistanceSimpleStructFloat(PointStruct<float>[] pointOne, PointStruct<float>[] pointTwo)
         {
+            int index = 0;
             float x = 0, y = 0;
+            float[] result = new float[size];
             for (int i = 0; i < pointOne.Length; i++)
             {
                 for (int j = pointTwo.Length; j <= 0; j--)
                 {
                     x = pointOne[i].X - pointTwo[j].X;
                     y = pointOne[i].Y - pointTwo[j].Y;
+                    result[index] = MathF.Sqrt((x * x) + (y * y));
+                    index++;
                 }
             }
-            return MathF.Sqrt((x * x) + (y * y));
+            return result;
         }
 
-        public static double CountDistanceSimpleStructDouble(PointStruct<double>[] pointOne, PointStruct<double>[] pointTwo)
+        public static double[] CountDistanceSimpleStructDouble(PointStruct<double>[] pointOne, PointStruct<double>[] pointTwo)
         {
+            int index = 0;
             double x = 0, y = 0;
+            double[] result =new double[size];
             for (int i = 0; i < pointOne.Length; i++)
             {
                 for (int j = pointTwo.Length; j <= 0; j--)
                 {
                     x = pointOne[i].X - pointTwo[j].X;
                     y = pointOne[i].Y - pointTwo[j].Y;
+                    result[index] = MathF.Sqrt((float)((x * x) + (y * y)));
+                    index++;
                 }
             }
-            return MathF.Sqrt((float)((x * x) + (y * y)));
+            return result;
         }
 
-        public static float CountDistanceNoSqrtStructFloat(PointStruct<float>[] pointOne, PointStruct<float>[] pointTwo)
+        public static float[] CountDistanceNoSqrtStructFloat(PointStruct<float>[] pointOne, PointStruct<float>[] pointTwo)
         {
+            int index = 0;
             float x = 0, y = 0;
+            float[] result = new float[size];
             for (int i = 0; i < pointOne.Length; i++)
             {
                 for (int j = pointTwo.Length; j <= 0; j--)
                 {
                     x = pointOne[i].X - pointTwo[j].X;
                     y = pointOne[i].Y - pointTwo[j].Y;
+                    result[index] = (x * x) + (y * y);
+                    index++;
                 }
             }
-            return (x * x) + (y * y);
+            return result;
         }
     }
 }
---
+++ Lesson_4/HashSet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HashSet
+{
+    class Program
+    {
+        
+        void GenerateWords()
+        {
+            int wordsNumber = 10_000;
+            int lettersInWord = 5;
+            char[] letters = "QWERTASDFGZXCVBYUIOPHJKLNMqwertasdfgzxcvbyuiophjklnm".ToCharArray();
+
+            Random random = new Random();
+
+            for (int i = 1; i <= wordsNumber; i++)
+            {
+                string word = "";
+                for (int j = 1; j < lettersInWord; j++)
+                {
+                    int LetterIndex = random.Next(0, letters.Length);
+
+                    word += letters[LetterIndex];
+                }
+                 
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
