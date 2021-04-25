15a6d2fe3f208eb48e402546f3006025662a57e8
Kolodinsky <Photon.74@gmail.com>
2021-04-25 23:30:07 +0300

    Try Tree

---
+++ Lesson_4/BST/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.AddItem(10);
+            tree.AddItem(7);
+            tree.AddItem(9);
+            tree.AddItem(12);
+            tree.AddItem(6);
+            tree.AddItem(14);
+            tree.AddItem(11);
+            tree.AddItem(3);
+            tree.AddItem(4);
+        }
+    }
+}
---
+++ Lesson_4/BST/Tree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST
+{
+    class Tree : ITree
+    {
+        public Node Root { get; private set; }        
+        public int Count { get; private set; }
+
+        public void AddItem(int value)
+        {            
+            var node = new Node { Value = value };
+            if (Root == null)
+            {
+                Root = node;
+                Count++;
+                return;
+            }
+            
+            var current = Root;
+            while (current != null)
+            {
+                if (current.Value < node.Value)
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = node;
+                    }
+                    else
+                    {
+                        current = current.Left;
+                        AddItem(value);
+                    }
+                }
+                else
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = node;
+                    }
+                    else
+                    {
+                        current = current.Right;
+                        AddItem(value);
+                    }
+                }
+            }
+            Count++;
+        }
+
+        public Node GetNodeByValue(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node GetRoot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItem(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
