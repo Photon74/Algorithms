f3d9ad7c5cad3ea88873608449b8272812f2b624
Kolodinsky <Photon.74@gmail.com>
2021-05-04 19:16:22 +0300

    Граф - 2

--- Lesson_6/Graf/Graf.cs
+++ Lesson_6/Graf/Graf.cs
@@ -18,6 +18,27 @@ namespace Graf
             Nodes.Add(new Node(nodeValue));
         }
 
+        public Node GetNodeByValue(int nodeValue)
+        {
+            foreach (var node in Nodes)
+            {
+                if (node.Value == nodeValue)
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
 
+        public void AddEdge(int firstNodeValue, int secondNodeValue, int weight)
+        {
+            var firstNode = GetNodeByValue(firstNodeValue);
+            var secondNode = GetNodeByValue(secondNodeValue);
+            if (firstNode != null && secondNode != null)
+            {
+                firstNode.AddEdge(secondNode, weight);
+                secondNode.AddEdge(firstNode, weight);
+            }
+        }
     }
 }
