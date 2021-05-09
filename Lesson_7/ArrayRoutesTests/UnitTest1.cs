a2583690bc6ee071b047f1d6cd1a8c0d322cc751
Kolodinsky <Photon.74@gmail.com>
2021-05-09 12:00:12 +0300

    -

--- Lesson_7/ArrayRoutesTests/UnitTest1.cs
+++ Lesson_7/ArrayRoutesTests/UnitTest1.cs
@@ -26,7 +26,7 @@ namespace ArrayRoutesTests
         [TestCase(3, 3, 6, TestName = "Arr[3,3]; Expected = 6 (With Obstacles)")]
         [TestCase(4, 4, 0, TestName = "Arr[4,4]; Expected = 0 (With Obstacles)")]
         [TestCase(5, 5, 6, TestName = "Arr[5,5]; Expected = 6 (With Obstacles)")]
-        [TestCase(6, 6, 32, TestName = "Arr[6,6]; Expected = 32")]
+        [TestCase(6, 6, 32, TestName = "Arr[6,6]; Expected = 32 (With Obstacles)")]
         public void Array_With_Obstacles_Test(int v1, int v2, int expected)
         {
             int actual = ArrayRoutesWithObstacles.CountRoutes(v1 - 1, v2 - 1);
