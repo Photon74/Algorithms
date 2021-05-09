853ec259a1963fcff902c8d9170d5fe65a0191b0
Kolodinsky <Photon.74@gmail.com>
2021-05-09 11:49:04 +0300

    -

--- Lesson_7/NumberOfRoutes/Program.cs
+++ Lesson_7/NumberOfRoutes/Program.cs
@@ -12,13 +12,14 @@ namespace NumberOfRoutes
 
             int result = ArrayRoutesWithoutObstacles.CountRoutes(A - 1, B - 1);
 
-            Console.WriteLine($"Кол-во выполненных рекурсивных вызовов: {ArrayRoutesWithoutObstacles.i}");
-            Console.WriteLine($"Кол-во маршрутов в правый нижний угол массива размерностью {A} на {B} без препятствий = {result}\n");
+            Console.WriteLine($"Кол-во выполненных рекурсивных вызовов: {ArrayRoutesWithoutObstacles.i}\n");
+            Console.WriteLine($"Кол-во маршрутов в правый нижний угол массива" +
+                $" размерностью {A} на {B} без препятствий = {result}\n");
 
             result = ArrayRoutesWithObstacles.CountRoutes(A - 1, B - 1);
 
-            Console.WriteLine($"Кол-во выполненных рекурсивных вызовов: {ArrayRoutesWithoutObstacles.i}");
-            Console.WriteLine($"Кол-во маршрутов в правый нижний угол массива размерностью {A} на {B} с препятствиями = {result}");
+            Console.WriteLine($"Кол-во маршрутов в правый нижний угол массива" +
+                $" размерностью {A} на {B} с препятствиями = {result}");
 
             Console.ReadLine();
         }
