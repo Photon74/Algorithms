a1a81592850491982d8be16a884e93267dade48f
Kolodinsky <Photon.74@gmail.com>
2021-05-09 16:52:47 +0300

    BucketSort готов

---
+++ Lesson_8/Bucketsort/BucketSortClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucketSort
+{
+    public class BucketSortClass
+    {
+        public static int[] BucketSortMethod(int[] a)
+        {
+            List<int>[] buckets = new List<int>[5];
+
+            for (int i = 0; i < buckets.Length; ++i)
+                buckets[i] = new List<int>();
+
+            int minValue = a[0];
+            int maxValue = a[0];
+
+            for (int i = 1; i < a.Length; ++i)
+            {
+                if (a[i] < minValue)
+                    minValue = a[i];
+                else if (a[i] > maxValue)
+                    maxValue = a[i];
+            }
+
+            double numRange = maxValue - minValue;
+
+            for (int i = 0; i < a.Length; ++i)
+            {
+                int bucketIndex = (int)Math.Round((a[i] - minValue) / numRange * (buckets.Length - 1));
+
+                buckets[bucketIndex].Add(a[i]);
+            }
+
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                buckets[i].Sort();
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                for (int j = 0; j < buckets[i].Count; ++j)
+                {
+                    a[index++] = buckets[i][j];
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ Lesson_8/Bucketsort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static BucketSort.BucketSortClass;
+
+namespace BucketSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayToSort = GetRandomArray(50, 1, 100);
+            Console.WriteLine("Начальный массив: " + string.Join(", ", arrayToSort) + "\n");
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ", BucketSortMethod(arrayToSort)));
+            Console.ReadLine();
+        }
+
+        static int[] GetRandomArray(int arraySize, int minValue, int maxValue)
+        {
+            var random = new Random();
+            var randomArray = new int[arraySize];
+            for (var i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = random.Next(minValue, maxValue);
+            }
+
+            return randomArray;
+        }
+    }
+}
